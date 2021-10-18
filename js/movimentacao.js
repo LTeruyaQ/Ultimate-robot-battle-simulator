de481120aec92a41e08f30b6b840b50e24463e8a
L.Teruya <leandro_tq02@hotmail.com>
2021-10-18 19:05:47 -0300

    Tentantiva de colisão

--- js/movimentacao.js
+++ js/movimentacao.js
@@ -84,39 +84,39 @@
   window.addEventListener('keydown', function (e) {
     const nomeKey = e.key;
 
-    if (map[38] && map[37]) {
+    if (map[38] && map[37]) 
       naves[0].img = AnimationDiagonalSprites1('38', '37');
-    } else if (map[37] && map[40]) {
+     else if (map[37] && map[40]) 
       naves[0].img = AnimationDiagonalSprites1('37', '40');
-    } else if (map[40] && map[39]) {
+     else if (map[40] && map[39]) 
       naves[0].img = AnimationDiagonalSprites1('40', '39');
-    } else if (map[39] && map[38]) {
+     else if (map[39] && map[38]) 
       naves[0].img = AnimationDiagonalSprites1('39', '38');
-    } else if (map[38]) {
+     else if (map[38]) 
       naves[0].img = AnimationSprites1('38');
-    } else if (map[37]) {
+     else if (map[37]) 
       naves[0].img = AnimationSprites1('37');
-    } else if (map[40]) {
+     else if (map[40]) 
       naves[0].img = AnimationSprites1('40');
-    } else if (map[39]) {
+     else if (map[39]) 
       naves[0].img = AnimationSprites1('39');
-    }
+    
   });
 
   //Movimenta nave1
   function moverSprites1() {
-    if (esquerda && !direita) {
+    if (esquerda && !direita) 
       player1.posX -= player1.velocidade;
-    }
-    if (direita && !esquerda) {
+    
+    if (direita && !esquerda) 
       player1.posX += player1.velocidade;
-    }
-    if (cima && !baixo) {
+    
+    if (cima && !baixo) 
       player1.posY -= player1.velocidade;
-    }
-    if (baixo && !cima) {
+    
+    if (baixo && !cima) 
       player1.posY += player1.velocidade;
-    }
+    
 
     player1.posX = Math.max(0, Math.min(cnv.width - player1.width, player1.posX));
     player1.posY = Math.max(0, Math.min(cnv.height - player1.height, player1.posY));
@@ -166,39 +166,39 @@
   window.addEventListener('keydown', function (e) {
     const nomeKey = e.key;
 
-    if (map[87] && map[65]) { 
+    if (map[87] && map[65])  
       naves[1].img = AnimationDiagonalSprites2('87', '65');
-    } else if (map[65] && map[83]) {
+     else if (map[65] && map[83]) 
       naves[1].img = AnimationDiagonalSprites2('65', '83');
-    } else if (map[83] && map[68]) {
+     else if (map[83] && map[68]) 
       naves[1].img = AnimationDiagonalSprites2('83', '68');
-    } else if (map[68] && map[87]) {
+     else if (map[68] && map[87]) 
       naves[1].img = AnimationDiagonalSprites2('68', '87');
-    } else if (map[87]) {
+     else if (map[87]) 
       naves[1].img = AnimationSprites2('87');
-    } else if (map[65]) {
+     else if (map[65]) 
       naves[1].img = AnimationSprites2('65');
-    } else if (map[83]) {
+     else if (map[83]) 
       naves[1].img = AnimationSprites2('83');
-    } else if (map[68]) {
+     else if (map[68]) 
       naves[1].img = AnimationSprites2('68');
-    }
+    
   });
 
   //movimenta as nave2
   function moverSprites2() {
-    if (a && !d) {
+    if (a && !d) 
       player2.posX -= player2.velocidade;
-    }
-    if (d && !a) {
+    
+    if (d && !a) 
       player2.posX += player2.velocidade;
-    }
-    if (w && !s) {
+    
+    if (w && !s) 
       player2.posY -= player2.velocidade;
-    }
-    if (s && !w) {
+    
+    if (s && !w) 
       player2.posY += player2.velocidade;
-    }
+    
 
     player2.posX = Math.max(0, Math.min(cnv.width - player2.width, player2.posX));
     player2.posY = Math.max(0, Math.min(cnv.height - player2.height, player2.posY));
@@ -218,6 +218,7 @@
     moverSprites1();
     moverSprites2();
     exibirSprites();
+    combat(player1.posX, player1.posY, player2.posX, player2.posY);
   }
   atualizaTela();
 }());
